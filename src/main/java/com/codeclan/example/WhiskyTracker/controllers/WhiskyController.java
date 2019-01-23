9daabfed08f8cc818af52a9a3384ad548a5b6f17
Steven Kay <stevendkay@gmail.com>
2019-01-23 14:42:28 +0000

    got whiskies by year

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,25 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/whiskies")
+public class WhiskyController {
+
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
+	@GetMapping("/year/{year}")
+	public List<Whisky> getWhiskiesByYear(@PathVariable Integer year) {
+		return whiskyRepository.getWhiskyByYear(year);
+	}
+
+}
